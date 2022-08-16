7d70e1f3d60a997cd73090401a9c6141e60ecd3b
k-constantin <k-constantin@mail.ru>
2022-08-16 18:27:53 +0300

    changed file task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
+  result = ((arr1.length == arr2.length) && (arr1.every((v, i) => v === arr2[i])));
+ 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(num => num > 0).filter(num => num % 3 === 0).map(num => num * 10);
 
   return resultArr; // array
 }
