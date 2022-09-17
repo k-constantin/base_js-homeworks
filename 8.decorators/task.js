35a52fa5259d90ce5a4f277d45b13ac0cbd29f5a
k-constantin <k-constantin@mail.ru>
2022-09-17 20:23:14 +0300

    changed task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,46 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.find(item => item.hash === hash); 
+      if (objectInCache) { 
+          console.log("Из кэша: " + objectInCache.value);
+          return ("Из кэша: " + objectInCache.value);
+      } 
+        let value = func.call(this, ...args); 
+      cache.push({hash, value}) ;
+      if (cache.length > 5) { 
+        cache.shift(); 
+      }
+      console.log("Вычисляем: " + value);
+      return "Вычисляем: " + value;
+
+        
+  }
+  return wrapper;
+ 
 }
 
+const sum = (...args) => args.reduce((acc, item) => acc + item, 0);
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let nextCall = true;
+  let interval;
+  function wrapper(...args) {
+    wrapper.allCount += 1;
+    if (nextCall) {
+      func(...args);
+      nextCall = false;
+    }
+    clearTimeout(interval);
+    interval = setTimeout(() => {
+      func.call(this, ...args);
+      wrapper.count += 1;
+      nextCall = true;
+    }, 3000);
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
 }
+
